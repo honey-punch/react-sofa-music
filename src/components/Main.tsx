172526cb419615608e0cb725e217ff9a7c2b4646
mad <mad@jangjicheol.local>
2023-01-31 17:43:31 +0900

    main image fade in & fade out 구현중

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import 'css/Main.css';
+import jacket1 from 'components/img/jacket1.jpg';
+import jacket2 from 'components/img/jacket2.jpg';
+import jacket3 from 'components/img/jacket3.jpg';
+import { useState } from "react";
 
 export default function Main() {
+  const imgs_list: NodeListOf<Element> = document.querySelectorAll(".main-img");
+  const imgs_array: Array<HTMLElement> = Array.prototype.slice.call(imgs_list);
+
+  const time = 2000;
+  let i = 0;
+
+  function fadeInAndOut(): void {
+    imgs_array[i].classList.add('fade');
+    setTimeout(() => {imgs_array[i].classList.remove('fade')}, time)
+
+    i++;
+
+    if (i == imgs_array.length) {
+      i = 0;
+    }
+  }
+
+  function startInterval() {
+    fadeInAndOut()
+      return setInterval(() => {
+        fadeInAndOut();
+      }, time)
+  }
+
+  startInterval();
+  
+
   return (
     <main className='main'>
-      
+      <img className='main-img' src={jacket1} alt="img" />
+      <img className='main-img' src={jacket2} alt="img" />
+      <img className='main-img' src={jacket3} alt="img" />
     </main>
   )
 }
