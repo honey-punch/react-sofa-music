9e9ef23398255aa5621e60cc05d7791743da3d68
mad <mad@jangjicheol.local>
2023-02-01 17:45:32 +0900

    fade in & fade out 구현중2

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,38 +6,19 @@ import jacket3 from 'components/img/jacket3.jpg';
 import { useState } from "react";
 
 export default function Main() {
-  const imgs_list: NodeListOf<Element> = document.querySelectorAll(".main-img");
-  const imgs_array: Array<HTMLElement> = Array.prototype.slice.call(imgs_list);
+  const imgBox_list: NodeListOf<Element> = document.querySelectorAll(".img-box");
+  const imgBox_array: Array<HTMLElement> = Array.prototype.slice.call(imgBox_list);
 
-  const time = 2000;
-  let i = 0;
+  const [fadeState, setFadeState] = useState(false);
 
-  function fadeInAndOut(): void {
-    imgs_array[i].classList.add('fade');
-    setTimeout(() => {imgs_array[i].classList.remove('fade')}, time)
-
-    i++;
-
-    if (i == imgs_array.length) {
-      i = 0;
-    }
-  }
-
-  function startInterval() {
-    fadeInAndOut()
-      return setInterval(() => {
-        fadeInAndOut();
-      }, time)
-  }
-
-  startInterval();
+  setTimeout(() => {setFadeState(true)}, 2000);
   
 
   return (
     <main className='main'>
-      <img className='main-img' src={jacket1} alt="img" />
-      <img className='main-img' src={jacket2} alt="img" />
-      <img className='main-img' src={jacket3} alt="img" />
+        <a href="#" className={fadeState ? 'img-box current' : 'img-box'}><img className='main-img' src={jacket1} alt="img" /></a>
+        <a href="#" className={fadeState ? 'img-box current' : 'img-box'}><img className='main-img' src={jacket1} alt="img" /></a>
+        <a href="#" className={fadeState ? 'img-box current' : 'img-box'}><img className='main-img' src={jacket1} alt="img" /></a>
     </main>
   )
 }
