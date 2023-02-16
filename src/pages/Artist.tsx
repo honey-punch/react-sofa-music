65fa0cd95f5595709989776d9d1f83535283cfd3
mad <mad@jangjicheol.local>
2023-02-16 10:24:03 +0900

    solved console error

--- src/pages/Artist.tsx
+++ src/pages/Artist.tsx
@@ -9,7 +9,7 @@ import ringo from 'pages/img/4.ringo.png';
 
 type members = 'john' | 'paul' | 'george' | 'ringo';
 
-export default function MainController() {
+export default function Artist() {
   const [linkState, setLinkState] = useState({
     john: false,
     paul: false,
@@ -30,7 +30,6 @@ export default function MainController() {
     });
   }
   
-
   return (
     <main className='main'>
       <ul className='artist__ul'>
@@ -38,28 +37,28 @@ export default function MainController() {
           <img className='artist__img' src={john} alt="john" />
           <Link className={linkState.john ? 'artist__link active' : 'artist__link'} to='/artist/1'>
             <h2 className='artist__name'>John Lennon</h2>
-            <a className='artist__more' href="#">more</a>
+            <p className='artist__more'>more</p>
           </Link>
         </li>
         <li className='artist__li' onMouseEnter={() => mouseEnter('paul')} onMouseLeave={() => mouseLeave('paul')}>
           <img className='artist__img' src={paul} alt="paul" />
           <Link className={linkState.paul ? 'artist__link active' : 'artist__link'} to='/artist/2'>
             <h2 className='artist__name'>Paul McCartney</h2>
-            <a className='artist__more' href="#">more</a>
+            <p className='artist__more'>more</p>
           </Link>
         </li>
         <li className='artist__li' onMouseEnter={() => mouseEnter('george')} onMouseLeave={() => mouseLeave('george')}>
           <img className='artist__img' src={george} alt="george" />
           <Link className={linkState.george ? 'artist__link active' : 'artist__link'} to='/artist/3'>
             <h2 className='artist__name'>George Harrison</h2>
-            <a className='artist__more' href="#">more</a>
+            <p className='artist__more'>more</p>
           </Link>
         </li>
         <li className='artist__li' onMouseEnter={() => mouseEnter('ringo')} onMouseLeave={() => mouseLeave('ringo')}>
           <img className='artist__img' src={ringo} alt="ringo" />
           <Link className={linkState.ringo ? 'artist__link active' : 'artist__link'} to='/artist/4'>
             <h2 className='artist__name'>Ringo Starr</h2>
-            <a className='artist__more' href="#">more</a>
+            <p className='artist__more'>more</p>
           </Link>
         </li>
       </ul>
