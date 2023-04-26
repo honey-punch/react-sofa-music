172ebea23c8b7d2a48ffba6689889799e951e04e
mad <mad@jangjicheol.local>
2023-04-26 10:27:44 +0900

    react-router-dom 공부

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -5,15 +5,18 @@ import Main from 'components/Main'
 import Footer from 'components/Footer'
 import Artist from 'pages/Artist';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Routers from 'routes/routers'
 
 export default function AppLayout() {
+    // const routers = [...Routers]
+    
     return (
         <div className='app-layout'>
             <BrowserRouter>
                 <Header />
-                {/* 연결되는 페이지가 많아지면 route를 따로 관리할 것
-                {routes.map((route) => <Route key={route.key} path={route.path} element={route.element} />) } */}
                 <Routes>
+                    {/* 연결되는 페이지가 많아지면 route를 따로 관리할 것
+                    {routers.map((route) => <Route key={route.key} path={route.path} element={<route.element />}></Route>)} */}
                     <Route path='/react-sofa-music' element={<Main />}></Route>
                     <Route path='/artist' element={<Artist />}></Route>
                 </Routes>
--- src/routes/routers.ts
+++ src/routes/routers.ts
@@ -1,17 +1,24 @@
 import { type } from 'os';
+import Main from 'components/Main'
+import Artist from 'pages/Artist'
 
 // 연결되는 페이지가 많아지면 route를 따로 관리할 것
 type route = {
   key: number,
   path: string, // '/경로'
-  element: string, // '<컴포넌트 이름> -- 위에 import해서 쓸것',
+  element: React.ComponentType<any>, // '컴포넌트 이름 -- 위에 import해서 쓸것',
 }
 
 const routes: Array<route> = [
   {
     key: 0,
-    path: '/경로',
-    element: '<컴포넌트 이름> -- 위에 import해서 쓸것',
+    path: '/react-sofa-music',
+    element: Main,
+  },
+  {
+    key: 1,
+    path: '/artist',
+    element: Artist,
   }
 ];
 
